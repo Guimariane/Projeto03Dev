db54421fa3fd436b346785727293cbb72f6eaace
Guimariane <ingrid.mariane.araujo@gmail.com>
2022-06-17 20:43:26 -0300

    Cadastro das Rotas
    Registro das rotas dos novos componentes

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,10 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 import Login from '/src/components/views/user/PgLogin.vue';
 import Criar from '/src/components/views/user/CriarConta.vue';
+import Dashboard from '/src/components/views/viewslogado/DashBoard.vue';
+import ListaPainel from '/src/components/views/viewslogado/ListaPainel.vue';
+import Registro from '/src/components/views/viewslogado/RegistroPainel.vue';
+import Mensal from '/src/components/views/viewslogado/GeracaoMensal.vue';
 
 const routes = [{
         path: '/entrar',
@@ -10,6 +14,22 @@ const routes = [{
     {
         path: '/criar',
         component: Criar
+    },
+    {
+        path: '/dashboard',
+        component: Dashboard
+    },
+    {
+        path: '/lista',
+        component: ListaPainel
+    },
+    {
+        path: '/registropainel',
+        component: Registro
+    },
+    {
+        path: '/geracaomensal',
+        component: Mensal
     }
 ]
 
