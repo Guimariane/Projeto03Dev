65bfdbe200494375907edc2377d8fce42eabcbf7
Guimariane <ingrid.mariane.araujo@gmail.com>
2022-06-22 19:56:36 -0300

    v1 Geração Mensal
    Primeira versão da página de geração mensal

--- src/components/views/viewslogado/GeracaoMensal.vue
+++ src/components/views/viewslogado/GeracaoMensal.vue
@@ -1,5 +1,40 @@
 <template>
-  <div></div>
+    <div>
+      <Form @submit="registrar">
+        <div>
+          <label for="">Unidade Geradora</label>
+          <br>
+          <Select name="nome" v-model="painel.apelido"></Select>
+        </div>
+        <br>
+        <div>
+          <label for="">Mês/ Ano</label>
+          <br>
+          <Field type="date"/>
+        </div>
+        <br>
+        <div>
+          <label for="">Marca</label>
+          <br>
+          <Field name="marca" v-model="painel.marca"/>
+        </div>
+        <br>
+        <div>
+          <label for="">Modelo</label>
+          <br>
+          <Field name="modelo" v-model="painel.modelo"/>
+        </div>
+        <br>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" name="ativo" v-model="painel.ativo">
+          <label class="form-check-label" for="exampleCheck1">Ativo</label>
+        </div>
+        <br>
+        <div class="md-5">
+        <button type="submit" class="btn btn-primary">Salvar</button>
+        </div>
+      </Form>
+    </div>
 </template>
 
 <script>
