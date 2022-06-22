fe0eeed3e3bb598155d8140f93216a8088280b98
Guimariane <ingrid.mariane.araujo@gmail.com>
2022-06-22 19:57:19 -0300

    Mudanças Funções
    Mudanças nas funções de excluir e editar das páginas de registro e lista do painel

--- src/components/views/viewslogado/ListaPainel.vue
+++ src/components/views/viewslogado/ListaPainel.vue
@@ -1,13 +1,81 @@
 <template>
-  <div></div>
+  <div>
+    <h3>Lista de Unidades</h3>
+    <div>
+      <table>
+        <thead>
+          <th>ID</th>
+          <th>Apelido</th>
+          <th>Local</th>
+          <th>Marca</th>
+          <th>Modelo</th>
+        </thead>
+        <tbody v-for="(painel, indice) in this.paineis" :key="indice">
+          <td>{{indice + 1}}</td>
+          <td>{{painel.apelido}}</td>
+          <td>{{painel.local}}</td>
+          <td>{{painel.marca}}</td>
+          <td>{{painel.modelo}}</td>
+          <td>
+            <button type="submit" class="btn btn-primary" @click="editar(indice)">Editar</button>
+          </td>
+          <td>
+            <button type="submit" class="delete-btn" @click="excluir(indice)">Excluir</button>
+          </td>
+        </tbody>
+      </table>
+    </div>
+    <br>
+    <div class="md-5">
+      <button type="submit" class="btn btn-primary" @click="cadastrar">Nova Unidade</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  components: {},
+  data(){
+    return{
+      painel: {
+      },
+      paineis: [],
+    }
+  },
+  methods: {
+    editar(indice){
+      this.$router.push(`/registropainel/${indice}`);
+    },
+
+    cadastrar(){
+      this.$router.push(`/registropainel`);
+    },
+
+    excluir(indice){
+      localStorage.getItem('painel');
+      this.paineis.splice(indice, 1);
+      localStorage.setItem('painel',JSON.stringify(this.paineis));
+    }
+  },
+  mounted() {
+    this.paineis = localStorage.getItem("painel") ? JSON.parse(localStorage.getItem("painel")) : [];
+    console.log(this.paineis)
+},
 
 }
 </script>
 
 <style>
-
+thead{
+  background-color: #F5F5F5;
+  border-radius: 10px;
+}
+th{
+  padding-left: 5px;
+  padding-right: 20px;
+}
+td{
+  padding-left: 5px;
+  padding-right: 20px;
+}
 </style>
--- src/components/views/viewslogado/RegistroPainel.vue
+++ src/components/views/viewslogado/RegistroPainel.vue
@@ -1,13 +1,105 @@
 <template>
-  <div></div>
+  <div>
+    <card class="card">
+      <Form @submit="registrar">
+        <h3>Cadastro de Unidade Geradora</h3>
+        <br>
+        <br>
+        <div>
+          <label for="">Apelido</label>
+          <br>
+          <Field name="nome" v-model="painel.apelido"/>
+        </div>
+        <br>
+        <div>
+          <label for="">Local</label>
+          <br>
+          <Field name="local" v-model="painel.local"/>
+        </div>
+        <br>
+        <div>
+          <label for="">Marca</label>
+          <br>
+          <Field name="marca" v-model="painel.marca"/>
+        </div>
+        <br>
+        <div>
+          <label for="">Modelo</label>
+          <br>
+          <Field name="modelo" v-model="painel.modelo"/>
+        </div>
+        <br>
+        <div class="mb-3 form-check">
+          <input type="checkbox" class="form-check-input" id="exampleCheck1" name="ativo" v-model="painel.ativo">
+          <label class="form-check-label" for="exampleCheck1">Ativo</label>
+        </div>
+        <br>
+        <div class="md-5">
+        <button type="submit" class="btn btn-primary">Salvar</button>
+        </div>
+      </Form>
+    </card>
+  </div>
 </template>
 
 <script>
+import { Form, Field } from 'vee-validate';
 export default {
+  components: {
+    Form,
+    Field
+  },
+  data() {
 
-}
+    return{
+      painel: {
+        ativo: false
+      },
+      paineis: [],
+    }
+  },
+  methods: {
+      registrar() { 
+        if(this.$route.params.indice) {
+          for(var i = 0; i < this.paineis; i++) {
+            if (this.paineis[i].indice == this.painel.indice) {
+              this.paineis[i] = this.painel
+            }
+          }
+          localStorage.setItem("painel", JSON.stringify(this.paineis));
+          alert("Registro alterado com sucesso");
+          this.$router.replace("/lista");
+        }
+        else {
+        const paineis = JSON.parse(localStorage.getItem("painel")) || [];
+        const newpainel = [...paineis, this.painel];
+        localStorage.setItem("painel", JSON.stringify(newpainel));
+        alert("Painel registrado com sucesso");
+        this.$router.replace("/lista");
+      }
+      }
+    
+  },
+
+    mounted() {
+        this.paineis = localStorage.getItem("painel") ? JSON.parse(localStorage.getItem("painel")) : [];
+        if (this.paineis !== null) {
+          this.paineis.forEach(energia => {
+            if(energia.indice == this.$route.params.indice) {
+              this.painel = energia
+            }
+          })
+        }
+      },
+    }
 </script>
 
 <style>
-
+h3{
+  font-weight: bolder;
+  font-family: Verdana, Geneva, Tahoma, sans-serif
+}
+.card{
+  background-color: #F5F5F5;
+}
 </style>
